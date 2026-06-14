0649ff74611c0d53d9e8c93568e9f7b4e94417de
ibnuhamdam <ibnu.hamdam16@gmail.com>
2026-06-14 23:50:39 +0700

    Fix: correct ESM export in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,9 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   images: {
     unoptimized: true,
   },
-  experimental: {
-    images: {
-      unoptimized: true,
-    },
-  },
-  basePath: '',
 };
 
-module.exports = nextConfig;
+export default nextConfig;
